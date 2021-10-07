e2c1ff330254436a790d8bb95a201e15428b8a3f
Yurii (AnomieRED) <anomiemio@gmail.com>
2021-10-07 17:37:05 +0300

    add some dependencies and fix function

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import weatherRouter from './routes/routes.js';
 
-const DB_URL = `mongodb+srv://admin:admin@cluster0.w1n80.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-const PORT = process.env.PORT ?? 8080;
+dotenv.config();
+
+const DB_CONNECT = process.env.DB_URL;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-//Use View Engine
 app.set('view engine', 'pug');
 
-//Middleware routes
 app.use(express.static('static'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/', weatherRouter);
 
-
 async function mongoStart() {
-   try {
-      await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-      app.listen(PORT, () => {
-         console.log(`Server has been started on port ${PORT}... `);
-      });
-   } catch (error) {
-      console.error(error);
-   }
+	try {
+		await mongoose.connect(DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true })
+		app.listen(PORT, () => {
+			console.log(`Server has been started on port ${PORT}... `);
+		});
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 mongoStart();
